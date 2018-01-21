9d39625f438d92238cdcb3865874a0e54c202894
osamu-s <osamus@star.odn.ne.jp>
2018-01-21 22:21:56 +0900

    modify from table fro csv

--- from_table.py
+++ from_table.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import json
+import csv
 from collections import OrderedDict
 
 def table_transpose(table_list):
@@ -9,35 +10,36 @@ def table_transpose(table_list):
 def tokenize(tbl_str, ttype,
              frame=0,
              junction_char='+',
-             vertical_char='|'):
+             separator=','):
+
     def pretty():
-        sep_idxs = [ i for i, c in enumerate(tbl_str[frame])
+        sep_idxs = [ i for i, c in enumerate(tbl_ln[frame])
                      if c == junction_char ]
         slices = [ (s+1, e-1) for s, e in zip([0]+sep_idxs, sep_idxs)[1:]]
-        return lambda ln: [ln[s:e].strip() for s, e in slices]
+        return ([ln[s:e].strip() for s, e in slices]
+                for ln in tbl_ln
+                if not len(ln.split()) == 1)
 
-    def mark_down():
-        return lambda ln: [col.strip() for col in ln.split(vertical_char)]
+    def from_csv():
+        return(csv.reader(tbl_ln, skipinitialspace=True))
 
-    f = { 'pretty': pretty,
-          'mark_down': mark_down }.get(ttype)()
-
-    return (f(ln) for ln in tbl_str
-            if not len(ln.split()) == 1)
+    tbl_ln = tbl_str.splitlines()
+    return { 'pretty': pretty,
+             'csv': from_csv }.get(ttype)()
 
 
 def from_table(table_str, ttype='pretty', key_dir='horizontal'):
-
-    tokens = tokenize(table_str.splitlines(), ttype)
-    tbl_list = table_transpose(tokens) if key_dir == 'vertical' else tokens
+    tokens = tokenize(table_str, ttype)
+    tbl_list = (table_transpose(tokens) if key_dir == 'vertical'
+                else tokens)
     keys = next(tbl_list)
     return [OrderedDict(zip(keys, ln)) for ln in tbl_list]
 
-md_table = """|               Col1               |  Col2   |          Col3          | Numeric Column |
-|----------------------------------|---------|------------------------|----------------|
-| Value 1                          | Value 2 | 123                    |           10.0 |
-| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
-| This is a row with only one cell |         |                        |                |
+
+csv_table = """Col1, Col2, Col3, Numeric Column
+Value 1, Value 2, 123, 10.0
+Separate, cols, with a tab or 4 spaces, -2,027.1
+This is a row with only one cell,,,
 """
 
 
@@ -63,7 +65,7 @@ def main():
     print (json.dumps(from_table(pretty_table), indent=2))
     print (json.dumps(from_table(vertical_pt, key_dir='vertical')[0],
                       indent=2))
-    print (from_table(md_table, ttype='mark_down'))
+    print (json.dumps(from_table(csv_table, ttype='csv'), indent=2))
 
 if __name__ == '__main__':
         main()
