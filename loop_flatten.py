dc92a4276ca919cf4cff8cfda7c0080802e6a773
osamu-s <osamus@star.odn.ne.jp>
2018-01-28 23:02:52 +0900

    add loop version

---
+++ loop_flatten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from collections import MutableSequence
+
+def flatten(mylist, levels=None):
+    def _core(ls, levels, ret):
+        if levels is not None:
+            levels = int(levels) -1
+        for element in ls:
+            if element in (None, 'None', 'null'):
+                # ignore undefined items
+                continue
+            elif (isinstance(element, MutableSequence) and
+                  (levels is None or levels >= 0) ):
+                    _core(element, levels, ret)
+            else:
+                ret.append(element)
+        return ret
+
+    return _core(mylist, levels, [])
+
+if __name__ == '__main__':
+
+    print (flatten([1, 2, 3, 4]))
+    print (flatten([1, [2, 3], 4]))
+    print (flatten([[1,2], [2, 3], [[4]]], levels=1))
+    print (flatten([[1,[]], [[[2], 3]], [[4]]]))
+    print (flatten([[1,[]], [[[2], 3]], [[4]]], levels=2))
